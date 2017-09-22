85672df641fa6dde1d277c27bfac4e37c76c9c23
Carl Vitullo <carl.vitullo@gmail.com>
2017-09-21 23:54:44 -0400

    Replace Exact logic with FilterControl

--- src/Exact.tsx
+++ src/Exact.tsx
@@ -1,7 +1,8 @@
 import React, { ChangeEvent } from "react";
 import PropTypes from "prop-types";
 
-import { Control } from "./filter.d";
+import FilterControl from "./FilterControl";
+import { ControlPair, FilterContext } from "./filter.d";
 
 const isActive = (value: string) => value !== "";
 
@@ -13,43 +14,17 @@ interface State {
   value: string;
 }
 
-export default class Exact extends React.Component<Props, State> {
-  static contextTypes = {
-    registerControl: PropTypes.func
-  };
-  props: {
-    name: string;
-  };
-  state = {
-    value: ""
-  };
-  updateFilter: () => {};
-  unregisterControl: () => {};
-
-  componentDidMount() {
-    let { unregister, update } = this.context.registerControl(this.control);
-    this.updateFilter = update;
-    this.unregisterControl = unregister;
-  }
-  componentWillUnmount() {
-    this.unregisterControl();
-  }
-  control = () => {
-    if (isActive(this.state.value)) {
-      return [this.props.name, this.state.value];
-    }
-    return undefined;
-  };
-  handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    let value = e.target.value;
-    this.setState({ value }, this.updateFilter);
-  };
+export default class NewExact<T> extends React.Component<Props, State> {
+  mapValuesToComparison = datum => datum[this.props.name];
+  compare = comparator => dataValue => dataValue === comparator;
   render() {
+    type TypedFilterControl = new () => FilterControl<T>;
+    const TypedFilterControl = FilterControl as TypedFilterControl;
     return (
-      <input
-        value={this.state.value}
-        onChange={this.handleChange}
-        {...this.props}
+      <TypedFilterControl
+        name={this.props.name}
+        mapValuesToComparison={this.mapValuesToComparison}
+        compare={this.compare}
       />
     );
   }
