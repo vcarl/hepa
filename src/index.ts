dd3fd66486de81b1ff4d62b3bf002069652cc7bf
Carl Vitullo <carl.vitullo@gmail.com>
2017-09-22 12:23:12 -0400

    Remove Range control

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import FilterProvider from "./FilterProvider";
-import Range from "./Range";
 import Fuzzy from "./Fuzzy";
 import Exact from "./Exact";
 import Search from "./Search";
 import Filter from "./Filter";
 import WithFilteredData from "./WithFilteredData";
 
-export {
-  FilterProvider,
-  Filter,
-  Exact,
-  Range,
-  Fuzzy,
-  Search,
-  WithFilteredData
-};
+export { FilterProvider, Filter, Exact, Fuzzy, Search, WithFilteredData };
