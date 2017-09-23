3b5e92b4882c55764b58dff48fd856fcc3620dab
Carl Vitullo <carl.vitullo@gmail.com>
2017-09-23 00:27:03 -0400

    Improve FilterProvider comments

--- src/FilterProvider.tsx
+++ src/FilterProvider.tsx
@@ -6,6 +6,8 @@ import { Control, ControlPair, Predicate, FilterContext } from "./filter.d";
 export type Subscriber<T> = (predicate: Predicate<T>) => boolean;
 
 export interface State<T> {
+  // The "any" is because individual controls specify what intermediate type
+  // is used. This component doesn't care and doesn't know what they are.
   controls: Array<Control<T, any>>;
   subscribers: Array<Subscriber<T>>;
 }
@@ -15,29 +17,7 @@ export interface State<T> {
  * flexible, composable way. Any filter control below it in the hierarchy will
  * get pass its predicate up, and any filter result will grab all predicates 
  * to filter data that's passed in.
- *
- * The context methods are
- *  - registerControl()
- *  - subscribe()
- *
- * `registerControl` takes a function as an argument and returns an object with
- * two attributes: `update` and `unregister` functions.
- *   
- *   The argument returns either undefined or a tuple of 2 functions. The first
- *   gets a data object and returns what values it wants to compare to, and the
- *   second is a curried function of:
- *   `filter value => output of first function => boolean`
- *   
- *   `update` is used for notifying FilterProvider that a filter value has
- *   changed annd the predicate needs to be recalculated.
- *   `unregister` function is used for removing the control (e.g. when a control
- *   unmounts).
  * 
- *
- * `subscribe` takes a callback that takes no arguments and returns no value
- * to call whenever the filter predicate changes. It returns a function for
- * unsubscribing, which also requires no arguments.
- *
  * There are a number of "control" components that make use of the
  * `context.registerControl` function, and a `Filter` component that consumes
  * `context.subscribe`.
@@ -48,6 +28,7 @@ export default class FilterProvider<T> extends React.Component<{}, State<T>> {
     subscribers: []
   };
 
+  // https://medium.com/@mtiller/react-contexts-in-typescript-1337abb2e5a7
   context: FilterContext<T, any>;
 
   static childContextTypes = {
@@ -68,8 +49,21 @@ export default class FilterProvider<T> extends React.Component<{}, State<T>> {
     this.updateSubscribers(nextState);
   }
   componentWillUnmount() {}
+  /**
+   * `registerControl` takes a function as an argument and returns an object with
+   * two attributes: `update` and `unregister` functions.
+   *   
+   *   The argument returns either undefined or a tuple of 2 functions. The first
+   *   gets a data object and returns what values it wants to compare to, and the
+   *   second is a curried function of:
+   *   `filter value => output of first function => boolean`
+   *   
+   *   `update` is used for notifying FilterProvider that a filter value has
+   *   changed annd the predicate needs to be recalculated.
+   *   `unregister` function is used for removing the control (e.g. when a control
+   *   unmounts).
+   */
   registerControl = (control: Control<T, any>) => {
-    // Prepend the new control to state.controls.
     this.setState(({ controls }) => ({ controls: [control, ...controls] }));
 
     return {
@@ -85,6 +79,9 @@ export default class FilterProvider<T> extends React.Component<{}, State<T>> {
   };
   updateSubscribers({ controls }) {
     function predicate(values: T) {
+      // Each control is a function that returns the current value in the
+      // control's state. Filter out any undefined and we're left with an
+      // array of Control tuples.
       const filteredControls = controls
         .map(cc => cc())
         .filter(x => x !== undefined) as ControlPair<T, any>[];
@@ -93,6 +90,11 @@ export default class FilterProvider<T> extends React.Component<{}, State<T>> {
 
     this.state.subscribers.forEach(subscriber => subscriber(predicate));
   }
+  /**
+   * `subscribe` takes a callback that takes no arguments and returns no value
+   * to call whenever the filter predicate changes. It returns a function for
+   * unsubscribing, which also requires no arguments.
+   */
   subscribe = (listener: Subscriber<T>) => {
     this.setState(({ subscribers }) => ({
       subscribers: [listener, ...subscribers]
