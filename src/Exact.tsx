fea8c4290334e7d666d52a873211da5e3837495f
Carl Vitullo <carl.vitullo@gmail.com>
2017-09-30 21:31:31 -0400

    Don't filter out name prop from Exact and Fuzzy
    Needs to get passed to the input!

--- src/Exact.tsx
+++ src/Exact.tsx
@@ -18,14 +18,13 @@ export default class Exact<T> extends React.Component<Props, {}> {
   compare = (filterValue: string) => (dataValue: string) =>
     dataValue === filterValue;
   render() {
-    const { name, ...props } = this.props;
     // There's a weird syntax for passing types to a generic react component
     // https://github.com/Microsoft/TypeScript/issues/3960#issuecomment-165330151
     type TypedFilterControl = new () => FilterControl<T, string>;
     const TypedFilterControl = FilterControl as TypedFilterControl;
     return (
       <TypedFilterControl
-        {...props}
+        {...this.props}
         mapValuesToComparison={this.mapValuesToComparison}
         compare={this.compare}
       />
